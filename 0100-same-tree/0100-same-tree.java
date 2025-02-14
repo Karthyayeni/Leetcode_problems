e0f66c7bab256888e13d9bb9720e5ca96d157a4b
Karthyayeni <karthyayenip.22cse@kongu.edu>
2025-02-14 14:30:37 +0530

    Time: 0 ms (100%), Space: 41 MB (82.43%) - LeetHub

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -15,14 +15,13 @@
  */
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        if(p  == null && q == null){
+        if(p == null && q == null){
             return true;
         }
-
-        if(p != null && q != null && p.val == q.val){
-            return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        else if(p != null && q != null){
+            return p.val == q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
         }
 
         return false;
     }
-}
+}                          
