11dd346108c872e89d9e4ea286ab5238175d4780
Karthyayeni <karthyayenip.22cse@kongu.edu>
2025-06-18 11:38:56 +0530

    Time: 30 ms (69.44%), Space: 51.6 MB (59%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -7,24 +7,27 @@ class Solution {
             if (i > 0 && nums[i] == nums[i-1]) {
                 continue;
             }
-            
-            int j = i + 1;
-            int k = nums.length - 1;
+
+            int j = i + 1, k = nums.length - 1;
 
             while (j < k) {
                 int total = nums[i] + nums[j] + nums[k];
 
-                if (total > 0) {
-                    k--;
-                } else if (total < 0) {
-                    j++;
-                } else {
+                if(total == 0){
                     res.add(Arrays.asList(nums[i], nums[j], nums[k]));
-                    j++;
 
-                    while (nums[j] == nums[j-1] && j < k) {
-                        j++;
-                    }
+                    while(j < k && nums[j] == nums[j + 1]) j++;
+                    while(j < k && nums[k] == nums[k - 1]) k--;
+
+                    j++;
+                    k--;
+
+                }
+                else if(total < 0){
+                    j++;
+                }
+                else{
+                    k--;
                 }
             }
         }
